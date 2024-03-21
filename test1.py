d4d3986087d1b4c7d0f47cacf0ffa64c17175299
Anirudh Sridhar <79989698+shotoyaar@users.noreply.github.com>
2024-03-21 13:18:10 +0530

    added pages as functions and changed login page logic

--- test1.py
+++ test1.py
@@ -2,8 +2,13 @@ import streamlit as st
 import pandas as pd
 import streamlit_authenticator as sat 
 import re
+import matplotlib.pyplot as plt
+from pathlib import Path
+from openpyxl import Workbook, load_workbook
 
 userDetails = pd.DataFrame(columns = ["Username", "Email", "Password"])
+cdir = Path.cwd().resolve()
+mod_path = (Path(__file__).parent).resolve()
 
 def unameval(uname):
     return bool(re.match(r"^[a-zA-Z0-9_-]{1, 20}$", uname))
@@ -51,43 +56,108 @@ def login():
                 st.write("Username not found. Please register before continuing.")
         
         return False
+    
+def hover(event, ax, pie, fig):
+    if event.inaxes == ax:
+        for wedge in pie:
+            wedge.set_alpha(0.5)
+        
+        wedge = pie[event.ind[0]]
+        wedge.set_alpha(1)
+        fig.canvas.draw_idle()
+    
+def analytics():
+    global cdir
+    global mod_path
+
+    data = pd.read_excel(mod_path/'data_set.xlsx')
+    totalCosts = data.groupby("Subsystem")["Total Cost"].sum()
+
+    fig, ax = plt.subplots()
+    pie1 = ax.pie(totalCosts, labels = totalCosts.index, autopct = "%1.1f%%")
+    ax.set_title("Subsystem Cost Distribution")
+
+    fig.canvas.mp1_connect('motion_notify_event', hover(ax, pie1, fig))
+
+    if st.button("Show Pie Chart"):
+        if st.session_state.get("show_chart"):
+            st.session_state["show_chart"] = False
+        else:
+            st.session_state["show_chart"] = True
+
+        if st.session_state["show_chart"]:
+            st.pyplot(fig)
+        else:
+            st.empty()
+
+    for subsystem in totalCosts.index:
+        if st.button(subsystem):
+            if st.session_state.get(subsystem):
+                st.session_state[subsystem] = False
+            else:
+                st.session_state[subsystem] = True
+
+            if st.session_state[subsystem]:
+                st.write(data[data["Subsystem"] == subsystem])
+            else:
+                st.empty()
+
+def write_to_xlsx(data):
+    try:
+        wb = load_workbook("data_set.xlsx")
+        sheet = wb.active
+
+    except FileNotFoundError:
+        wb = Workbook()
+        sheet = wb.active
+        sheet.append(["Line Num.", "Area of Commodity", "Assm / Part #", "Assembly Component", "Description", "Unit Cost", "Qty", "Material Cost", "Process Cost", "Fastener Cost", "Tooling Cost", "Details", "Page #", "Subsystem"])
+
+    sheet.append(data)
+    wb.save("data_set.xlsx")
+
+def inputform():
+    st.title("Adding elements into the Inventory")
+    data = pd.read_excel("data_set.xlsx")
+
+    line_no = len(data) + 1
+    area_of_commodity = st.text_input("Area of Commodity")
+    assm_part = st.text_input("Assm / Part #")
+    assembly_component = st.text_input("Assembly Component")
+    description = st.text_input("Description")
+    unit_cost = st.number_input("Unit Cost")
+    qty = st.number_input("Qty")
+    material_cost = st.number_input("Material Cost")
+    process_cost = st.number_input("Process Cost")
+    tooling_cost = st.number_input("Tooling Cost")
+    total_cost = unit_cost*qty + material_cost + process_cost + tooling_cost
+    details = st.text_input("Details")
+    pageno = 1
+    subsystem = st.selectbox("Subsystem", data["Subsystem"].unique())
+
+    if st.button("Submit"):
+        new_row = [line_no, area_of_commodity, assm_part, assembly_component, description, unit_cost, qty, material_cost, process_cost, tooling_cost, total_cost, details, pageno, subsystem]
+        write_to_xlsx(new_row)
+        st.success("Data written to data_set_2.xlsx")
 
 def main():
     st.title("Login/Register")
-
-    if not login():
+    
+    newDataframe = pd.DataFrame({"Username": "a", "Email": "email", "Password": "a"}, index = [0])
+    pd.concat([userDetails, newDataframe], ignore_index = True)
+    l = login()
+    if not l:
+        st.warning("Please register before you continue")
+    
+    if st.button("Register"):
         uname, pword, email = register()
 
-        if uname and pword and email:
-            global userDetails
-            userDetails = userDetails.append({"Username": uname, "Email": email, "Password": pword}, ignore_index = True)
+        if uname and pword and email and unameval(uname) and emailval(email) and pwordval(pword):
+            st.success("You have successfully registered. Please login to continue.")
+            # global userDetails
+            newDataframe = pd.DataFrame({"Username": uname, "Email": email, "Password": pword}, index = [0])
+            pd.concat([userDetails, newDataframe], ignore_index = True)
+    
 
-
-
-#with st.form("login_form"):
-#    uname = st.text_input("Username")
-#    pword = st.text_input("Password", type="password")    
-#    submit = st.form_submit_button("Submit")
-
-#    if submit:
-
-#        if uname not in userDetails:
-#            st.write("Username not found. Please register before continuing.")
-#            st.write("Please enter email and name to register.")
-#            email = st.text_input("Email")
-#            name = st.text_input("Name")
-#            submit2 = st.form_submit_button("Register")
-
-#            if submit2:
-#                if unameval(uname) and emailval(email) and nameval(name) and pwordval(pword):
-#                    userDetails = userDetails.append({"Username": uname, "Email": email, "Password": pword}, ignore_index=True)
-                
-#                else:
-#                    st.write("Please re-enter your details.")
-        
-#        else:
-#            st.write("Welcome back!")
-#            st.write(userDetails[uname])
 
 
 main()
